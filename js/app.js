6dcf2ea42841eb43296cb7b1bc008792fddb6e62
Faruqoloyede <oloyedefaruq2@gmail.com>
2022-09-21 19:52:06 +0100

    mobile view

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const sideMenu = document.querySelector('.sideMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const main_items = document.querySelectorAll('.sideMenu a');
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+menu_items.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    sideMenu.style.display = 'block';
+    sideMenu.style.right = '0';
+}
+
+function close(){
+    sideMenu.style.right = '-100%';
+}
